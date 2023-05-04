e8318445be2878e03c0ae7e5c11b1ae3f9bfd74b
MuhammadBabarWaseem <mbabarwaseem@gmail.com>
2023-05-04 12:24:53 +0500

    API Improved!

--- src/pages/api/fetchtask.js
+++ src/pages/api/fetchtask.js
@@ -1,21 +1,26 @@
 import connectMongo from "../../../database/conn";
-import { Task } from "../../../model/Schema";
+import { StudentClass } from "../../../model/Schema";
 
 export default async function handler(req, res) {
     connectMongo().catch(error => res.json({ error: "Connection failed" }))
 
-    if (req.method === 'GET') {
+    if (req.method === 'POST') {
+        if (!req.body) return res.status(404).json({ error: 'Dont Have form Data' })
+        console.log("Yehh", req.body)
+        const { StudentClassId } = req.body
 
-        const tasks = await Task.find();
-        if (tasks) {
-            res.status(201).json({ status: true, tasks: tasks })
+
+        const checkExisting = await StudentClass.findOne({ _id: StudentClassId })
+        if (checkExisting) {
+            res.status(201).json({ status: true, data: checkExisting })
         }
         else {
-            res.status(201).json({ status: true, tasks: [] })
+            res.status(201).json({ status: true, data: "" })
+
         }
     }
 
     else {
-        res.status(500).json({ message: "HTTP Method not valid only GET Accepted" })
+        res.status(500).json({ message: "HTTP Method not valid only POST Accepted" })
     }
 }
